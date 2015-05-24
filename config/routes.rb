6069c0a10364c62de09130873193115da8cfc0f4
Jefferson Choi <jeffersonchoi@gmail.com>
2015-05-24 03:24:26 -0700

    created a table for users list, edited button for users list

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'welcome#index', as: :index_page
 
   get "users/" => "users#index"
   get "users/new" => "users#new", as: :new_user
